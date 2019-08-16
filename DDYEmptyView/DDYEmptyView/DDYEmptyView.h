e64fd5780b2bb2f14188c99a729f69e1bdd96c83
rain <634778311@qq.com>
2019-08-16 19:06:00 +0800

    创建工程

---
+++ DDYEmptyView/DDYEmptyView/DDYEmptyView.h
@@ -0,0 +1,70 @@
+#import <UIKit/UIKit.h>
+
+
+@interface DDYEmptyView : UIView
+
+/** 无数据占位自定义视图 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^customView)(UIView *customView);
+
+/** 无数据占位图片(自定义时设置无效) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^image)(UIImage *image);
+/** 无数据占位图片名称(自定义时设置无效) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^imageName)(NSString *imageName);
+/** 无数据占位标题(自定义时设置无效) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^title)(NSString *title);
+/** 无数据占位详细描述(自定义时设置无效) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^detail)(NSString *detail);
+/** 无数据占位按钮标题(自定义时设置无效) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionTitle)(NSString *actionTitle);
+/** 内容区域点击事件回调(自定义时设置无效) */
+@property (nonatomic, copy) void (^contentViewClickBlock)(void);
+/** 按钮点击事件回调(自定义时设置无效) */
+@property (nonatomic, copy) void (^actionButtonClickBlock)(void);
+
+/** 是否自动显隐(自定义时设置也有效) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^autoShow)(BOOL autoShow);
+
+/** 整体背景色 默认RGBA(250.0/255.0, 250.0/255.0, 250.0/255.0, 1.0) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^bgColor)(UIColor *bgColor);
+/** 图片大小 默认图片大小 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^imageSize)(CGSize imageSize);
+/** 图片与标题间距 默认20 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^imageTitleMargin)(CGFloat imageTitleMargin);
+/** 标题字体 默认16号字体 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^titleFont)(UIFont *titleFont);
+/** 标题字色 默认RGBA(0.3, 0.3, 0.3, 1.0) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^titleColor)(UIColor *titleColor);
+/** 标题与详细描述间距 默认20 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^titleDetailMargin)(CGFloat titleDetailMargin);
+/** 详细描述字体 默认14号字体 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^detailFont)(UIFont *detailFont);
+/** 详细描述字色 默认RGBA(0.5, 0.5, 0.5, 1.0) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^detailColor)(UIColor *detailColor);
+/** 详细描述与按钮间距 默认20 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^detailActionMargin)(CGFloat detailActionMargin);
+/** 按钮字体 默认14号字体 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionFont)(UIFont *actionFont);
+/** 按钮字色 默认RGBA(0.3, 0.3, 0.3, 1.0) */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionColor)(UIColor *actionColor);
+/** 按钮内边距 默认zero */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionInsets)(UIEdgeInsets actionInsets);
+/** 按钮背景色 默认clear */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionBackgroundColor)(UIColor *actionBackgroundColor);
+/** 按钮边色 默认clear */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionBorderColor)(UIColor *actionBorderColor);
+/** 按钮边宽 默认0 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionBorderWidth)(CGFloat actionBorderWidth);
+/** 按钮圆角 默认0 */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^actionCornerRadius)(CGFloat actionCornerRadius);
+
+/** 内容偏移调整 默认zero */
+@property (nonatomic, copy, readonly) DDYEmptyView *(^offset)(CGPoint offset);
+
+
+- (instancetype)init NS_UNAVAILABLE;
+
++ (instancetype)emptyView;
+
+
+@end
+
---
+++ DDYEmptyView/DDYEmptyView/DDYEmptyView.m
@@ -0,0 +1,248 @@
+#import "DDYEmptyView.h"
+#import "DDYEmptyConfig.h"
+
+#define DDYEmptyViewProperty(TYPE, NAME) \
+- (DDYEmptyView *(^)(TYPE))NAME {\
+    return ^DDYEmptyView *(TYPE NAME) {\
+        self.NAME##Value = NAME;\
+        return self;\
+    };\
+}\
+
+
+inline UIColor *ddyEmptyColor(int r, int g, int b) { return [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]; }
+
+@interface DDYEmptyView ()
+
+/** 无数据占位自定义视图 */
+@property (nonatomic, strong) UIView *customViewValue;
+
+/** 无数据占位图片 */
+@property (nonatomic, strong) UIImage *imageValue;
+/** 无数据占位图片名称 */
+@property (nonatomic, copy) NSString *imageNameValue;
+/** 无数据占位标题 */
+@property (nonatomic, copy) NSString *titleValue;
+/** 无数据占位详细描述 */
+@property (nonatomic, copy) NSString *detailValue;
+/** 无数据占位按钮标题*/
+@property (nonatomic, copy) NSString *actionTitleValue;
+
+
+/** 是否自动显隐(自定义时设置也有效) */
+@property (nonatomic, assign) BOOL autoShowValue;
+
+/** 整体背景色 默认RGBA(250.0/255.0, 250.0/255.0, 250.0/255.0, 1.0) */
+@property (nonatomic, strong) UIColor *bgColorValue;
+/** 图片大小 默认图片大小 */
+@property (nonatomic, assign) CGSize imageSizeValue;
+/** 图片与标题间距 默认20 */
+@property (nonatomic, assign) CGFloat imageTitleMarginValue;
+/** 标题字体 默认16号字体 */
+@property (nonatomic, strong) UIFont *titleFontValue;
+/** 标题字色 默认RGBA(0.3, 0.3, 0.3, 1.0) */
+@property (nonatomic, strong) UIColor *titleColorValue;
+/** 标题与详细描述间距 默认20 */
+@property (nonatomic, assign) CGFloat titleDetailMarginValue;
+/** 详细描述字体 默认14号字体 */
+@property (nonatomic, strong) UIFont *detailFontValue;
+/** 详细描述字色 默认RGBA(0.5, 0.5, 0.5, 1.0) */
+@property (nonatomic, strong) UIColor *detailColorValue;
+/** 详细描述与按钮间距 默认20 */
+@property (nonatomic, assign) CGFloat detailActionMarginValue;
+/** 按钮字体 默认14号字体 */
+@property (nonatomic, strong) UIFont *actionFontValue;
+/** 按钮字色 默认RGBA(0.3, 0.3, 0.3, 1.0) */
+@property (nonatomic, strong) UIColor *actionColorValue;
+/** 按钮内边距 默认zero */
+@property (nonatomic) UIEdgeInsets actionInsetsValue;
+/** 按钮背景色 默认clear */
+@property (nonatomic, strong) UIColor *actionBackgroundColorValue;
+/** 按钮边色 默认clear */
+@property (nonatomic, strong) UIColor *actionBorderColorValue;
+/** 按钮边宽 默认0 */
+@property (nonatomic, assign) CGFloat actionBorderWidthValue;
+/** 按钮圆角 默认0 */
+@property (nonatomic, assign) CGFloat actionCornerRadiusValue;
+
+/** 内容偏移调整 默认zero */
+@property (nonatomic, assign) CGPoint offsetValue;
+
+/** 默认显示内容视图 */
+@property (nonatomic, strong) UIView *contentView;
+/**  */
+@property (nonatomic, strong) UIImageView *imageView;
+/**  */
+@property (nonatomic, strong) UILabel *titleLabel;
+/**  */
+@property (nonatomic, strong) UILabel *detailLabel;
+/**  */
+@property (nonatomic, strong) UIButton *actionButton;
+
+
+@end
+
+@implementation DDYEmptyView
+
+// MARK:- 设置
+
+DDYEmptyViewProperty(UIView *, customView)
+DDYEmptyViewProperty(UIImage *, image)
+DDYEmptyViewProperty(NSString *, imageName)
+DDYEmptyViewProperty(NSString *, title)
+DDYEmptyViewProperty(NSString *, detail)
+DDYEmptyViewProperty(NSString *, actionTitle)
+
+DDYEmptyViewProperty(BOOL, autoShow)
+
+DDYEmptyViewProperty(UIColor *, bgColor)
+DDYEmptyViewProperty(CGSize, imageSize)
+DDYEmptyViewProperty(CGFloat, imageTitleMargin)
+DDYEmptyViewProperty(UIFont *, titleFont)
+DDYEmptyViewProperty(UIColor *, titleColor)
+DDYEmptyViewProperty(CGFloat, titleDetailMargin)
+DDYEmptyViewProperty(UIFont *, detailFont)
+DDYEmptyViewProperty(UIColor *, detailColor)
+DDYEmptyViewProperty(CGFloat, detailActionMargin)
+DDYEmptyViewProperty(UIFont *, actionFont)
+DDYEmptyViewProperty(UIColor *, actionColor)
+DDYEmptyViewProperty(UIColor *, actionBackgroundColor)
+DDYEmptyViewProperty(UIColor *, actionBorderColor)
+DDYEmptyViewProperty(CGFloat, actionBorderWidth)
+DDYEmptyViewProperty(CGFloat, actionCornerRadius)
+DDYEmptyViewProperty(CGPoint, offset)
+
+- (void)setImageNameValue:(NSString *)imageNameValue {
+    _imageNameValue = imageNameValue;
+    UIImage *img = [UIImage imageNamed:imageNameValue];
+    if (img) {
+        self.imageValue = img;
+    }
+}
+
+// MARK:- 懒加载
+
+- (UIView *)contentView {
+    if (!_contentView) {
+        _contentView = [[UIView alloc] init];
+    }
+    return _contentView;
+}
+
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] init];
+    }
+    return _imageView;
+}
+
+
+// MARK:- 声明周期
+
+- (instancetype)init {
+    if (self = [super init]) {
+        [self setAutoresizingMask:UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight];
+        [self addSubview:self.contentView];
+        [self.contentView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapContentView)]];
+        [self.contentView addSubview:self.imageView];
+        [self.contentView addSubview:self.titleLabel];
+        [self.contentView addSubview:self.detailLabel];
+        [self.contentView addSubview:self.actionButton];
+        [self.contentView setHidden:YES];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    if (self.superview && [self.superview isKindOfClass:[UIView class]]) {
+        self.frame = self.superview.bounds;
+        
+        CGFloat centerX = self.bounds.size.width/2.0 + self.offsetValue.x;
+        CGFloat centerY = self.bounds.size.height/2.0 + self.offsetValue.y;
+        
+        self.contentView.hidden = _customViewValue ? YES : NO;
+        
+        if (_customViewValue) {
+            self.customViewValue.center = CGPointMake(centerX, centerY);
+        } else {
+            self.contentView.center = CGPointMake(centerX, centerY);
+            [self layoutContentSubviews];
+        }
+    }
+    
+    
+}
+
+- (void)layoutContentSubviews {
+    
+    CGRect imageFrame = CGRectZero;
+    CGFloat imageTitleMargin = (self.imageValue && self.titleValue) ? 20 : 0;
+    CGRect titleFrame = CGRectZero;
+    CGFloat titleDetailMargin = (self.titleValue && self.detailValue) ? 20 : 0;
+    CGRect detailFrame = CGRectZero;
+    CGFloat detailActionMargin = (self.detailValue && self.actionTitleValue) ? 20 : 0;
+    CGRect actionFrame = CGRectZero;
+    
+    
+    
+    // 图片视图部分(即使超出范围也不缩放)
+    if (self.imageValue) {
+        self.imageView.image = self.imageValue;
+        CGFloat imgW = self.imageSizeValue.width ?: [DDYEmptyConfig defaultConfig].imageSize.width ?: self.imageValue.size.width;
+        CGFloat imgH = self.imageSizeValue.height ?: [DDYEmptyConfig defaultConfig].imageSize.height ?: self.imageValue.size.height;
+        imageFrame = CGRectMake(0, 0, imgW, imgH);
+    }
+    // 图片和标题
+    if (self.imageValue && self.titleValue) {
+        imageTitleMargin = self.imageTitleMarginValue ?: [DDYEmptyConfig defaultConfig].imageTitleMargin;
+    }
+    
+    if (self.titleValue) {
+        
+    }
+    
+    
+}
+
+// 构造器
+
++ (instancetype)emptyView {
+    return [[self alloc] init];
+}
+
+// MARK:- 事件响应
+
+- (void)tapContentView {
+    if (self.contentViewClickBlock) {
+        self.contentViewClickBlock();
+    }
+}
+
+
+// MARK:- 设置
+- (void)setImageViewAttributes:(void (^)(DDYEmptyView *, UIImageView *))attributes {
+    if (attributes) {
+        attributes(self, self.imageView);
+    }
+}
+
+- (void)show {
+    self.hidden = NO;
+    
+    self.backgroundColor = (_bgColorValue ?: [DDYEmptyConfig defaultConfig].backgroundColor) ?: ddyEmptyColor(250.0, 250.0, 250.0);
+    
+    [self.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    
+    if (_customViewValue) {
+        [self addSubview:self.customViewValue];
+    } else {
+        [self addSubview:self.contentView];
+        
+        
+        
+    }
+}
+
+@end
